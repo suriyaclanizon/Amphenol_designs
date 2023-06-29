cf13613495ad291aa2e8b1ff081509cfa5429012
suriyaclanizon <102014261+suriyaclanizon@users.noreply.github.com>
2023-06-29 21:11:33 +0530

    Dashboard screen update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -64,57 +64,57 @@ const Dashboard = () => {
             <div className="grid">
                 <div className="col-12 sm:col-6 md:col-4 xl:col-3 relative card p-0 m-2">
                     <img src={dash1} className='w-full' />
-                    <div className='absolute top-0  card-content'>
-                        <div className="cardcon">
+                    <div className='absolute dash-color-card'>
+                        <div className="dash-card-content">
                             <h2 className="head1">Zone 1</h2>
 
                             <p>09 Jun, 2023</p>
                             <h4 className="head2">3256</h4>
                         </div>
-                        <div className="cardDown1">
-                            <div className="cardLeft">
-                                <h3>5000</h3>
+                        <div className="dash-card-inner">
+                            <div className="dash-card-left">
+                                <h3 className="head5">5000</h3>
                                 <p>Target No</p>
                             </div>
-                            <div className="cardRight1">
-                                <h3>32</h3>
+                            <div className="dash-card-right">
+                                <h3 className="head5">32</h3>
                                 <p>QA Rejected</p>
                             </div>
                         </div>
                     </div>
                     <div className="relative">
 
-                        <div className='card-content1'>
+                        <div className='dash-down-card'>
 
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Sanden Vikas</h4>
                                     <p className="para1">2884-0850V</p>
                                     <p>San - 01</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">315</h3>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Tata Ficosa</h4>
                                     <p>06040301X028</p>
                                     <p>Vistra & Nexon line</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h4 className="head4">315</h4>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Rane TRW Steering  Systems Ltd</h4>
                                     <p>34299215B</p>
                                     <p>FCA Line</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">315</h3>
                                     <p>450</p>
                                 </div>
@@ -125,56 +125,56 @@ const Dashboard = () => {
 
                 <div className="col-12 sm:col-6 md:col-4 xl:col-3 relative card p-0 m-2">
                     <img src={dash2} className='w-full' />
-                    <div className='absolute top-0  card-content'>
-                        <div className="cardcon">
+                    <div className='absolute dash-color-card'>
+                        <div className="dash-card-content">
                             <h2 className="head1">Zone 2</h2>
                             <p>09 Jun, 2023</p>
                             <h4 className="head2">3256</h4>
                         </div>
-                        <div className="cardDown1">
-                            <div className="cardLeft">
-                                <h3>5000</h3>
+                        <div className="dash-card-inner">
+                            <div className="dash-card-left">
+                                <h3 className="head5">7000</h3>
                                 <p>Target No</p>
                             </div>
-                            <div className="cardRight">
-                                <h3>54</h3>
+                            <div className="dash-card-right">
+                                <h3 className="head5">54</h3>
                                 <p>QA Rejected</p>
                             </div>
                         </div>
                     </div>
                     <div className=" relative">
 
-                        <div className=' card-content1'>
+                        <div className=' dash-down-card'>
 
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">AUTOLIV INDIA PVT LTD</h4>
                                     <p>658872000B</p>
                                     <p>YTA LH line</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">315</h3>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Valeo-India</h4>
                                     <p>L353547</p>
                                     <p>SK216 Valeo</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h4 className="head4">315</h4>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Joysonsafety</h4>
                                     <p>1169193-AB</p>
                                     <p>Joy-01</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">315</h3>
                                     <p>450</p>
                                 </div>
@@ -185,56 +185,56 @@ const Dashboard = () => {
                 </div>
                 <div className="col-12 sm:col-6 md:col-4 xl:col-3 relative card p-0 m-2">
                     <img src={dash3} className='w-full' />
-                    <div className='absolute top-0  card-content'>
-                        <div className="cardcon">
+                    <div className='absolute dash-color-card'>
+                        <div className="dash-card-content">
                             <h2 className="head1">Zone 3</h2>
                             <p>09 Jun, 2023</p>
                             <h4 className="head2">3256</h4>
                         </div>
-                        <div className="cardDown1">
-                            <div className="cardLeft">
-                                <h3>5000</h3>
+                        <div className="dash-card-inner">
+                            <div className="dash-card-left">
+                                <h3 className="head5">8500</h3>
                                 <p>Target No</p>
                             </div>
-                            <div className="cardRight">
-                                <h3>48</h3>
+                            <div className="dash-card-right">
+                                <h3 className="head5">48</h3>
                                 <p>QA Rejected</p>
                             </div>
                         </div>
                     </div>
                     <div className="relative">
 
-                        <div className='card-content1'>
+                        <div className='dash-down-card'>
 
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Rane TRW Steering Systems Ltd</h4>
                                     <p>2884-0850V</p>
                                     <p>FCA Line</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">315</h3>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Varroc</h4>
                                     <p>06040301X028</p>
                                     <p>Varr - 02</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">600</h3>
                                     <p>450</p>
                                 </div>
                             </div>
-                            <div className="cardDown">
-                                <div className="cardLeft">
+                            <div className="dash-card-content1">
+                                <div className="dash-card-left">
                                     <h4 className="head3">Valeo-India</h4>
                                     <p>L359116</p>
                                     <p>SK216 Valeo</p>
                                 </div>
-                                <div className="cardRight">
+                                <div className="dash-card-right">
                                     <h3 className="head4">700</h3>
                                     <p>450</p>
                                 </div>
