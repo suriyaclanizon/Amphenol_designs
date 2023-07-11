a5a3fc2ebcd0050d5a4acf0eff73a98d329d9d5d
Ajitha1a <95858873+Ajitha1a@users.noreply.github.com>
2023-07-11 09:27:16 +0530

    master changes

--- src/components/Master.js
+++ src/components/Master.js
@@ -10,37 +10,22 @@ import { Button } from "primereact/button";
 
 const Master = () => { 
     const history = useHistory();
-
-    // const [activeIndex, setActiveIndex] = useState(0);
     const setActiveIndex = useStoreActions((actions) => actions.tabModel.setActiveIndex);
     const activeIndex = useStoreState((actions) => actions.tabModel.activeIndex);
-
-    // useEffect(() => {
-    //     setActiveIndex(0)
-    //     history.push('/allTrainee')
-    // }, []);
-
 
     const items = [
         { label: 'Store' },
         { label: 'WTP' },
         { label: 'Inward' },
-        // { label: 'All Trainee', command: () => history.push('/app/allTrainee') },
     ];
 
     return (
         <>
-         {/* <div className="card">
-        <Button className="btn" label="Edit"  /> */}
-       
-            <div className="Trainee-wrapper lg:mt-4">
+            <div className="Trainee-wrapper">
                 <div className="flex justify-content-between card">
-                    
-                <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
-                        <Button className="btn" label="Add" />
+                    <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
+                    <Button className="btn" label="Add" />
                 </div>
-                {/* <Route exact path="/app/allTrainee" component={AllTrainee}></Route>
-                <Route path="/app/allTrainee/addtrainee" component={AddTrainee}></Route> */}
                 {activeIndex === 0 && <Store />}
                 {activeIndex === 1 && <WTP />}
                 {activeIndex === 2 && <Inward />}
